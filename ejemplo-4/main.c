47fb88d11c897db939698943aa914401389d8e03
Andres Saavedra <andres.saavedra01@uceva.edu.co>
2025-03-17 21:05:42 +0000

    un commit nomas

--- ejemplo-4/main.c
+++ ejemplo-4/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//rax     rdi, rsi
 int sumar(int, int);
 
 int main (int numeroParametros, char ** argumentos){
@@ -8,9 +8,9 @@ int main (int numeroParametros, char ** argumentos){
      int resultado;
      int numero1=5;
      int numero2=6;
-
+     printf("el numero de parametros en la linea de comandos fue %d\r\n", numeroParametros);
      resultado = sumar(numero1, numero2);
-     printf("la suma de %d y %d en %d", numero1, numero2, resultado);
+     printf("la suma de %d y %d en %d\r\n", numero1, numero2, resultado);
 
     return(EXIT_SUCCESS);
 }
